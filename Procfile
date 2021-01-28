dac9abc281f7e26a4c81321521f26482fca1ea0c
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-01-28 14:15:00 -0300

    Move server.js to project root

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node -r esm src/server.js
+web: node -r esm server.js
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const { Server } = require('boardgame.io/server');
 import path from 'path';
 import serve from 'koa-static';
-const { Game } = require('./Game');
+const { Game } = require('./src/Game');
 
 const server = Server({ games: [Game] });
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, '../dist');
+const frontEndAppBuildPath = path.resolve(__dirname, './dist');
 server.app.use(serve(frontEndAppBuildPath))
 
 server.run(PORT, () => {
