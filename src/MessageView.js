91e82efd8f9e7c0c39b5959dacf6a16af269f689
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-01-31 12:35:04 -0300

    Show state of other players

--- src/MessageView.js
+++ src/MessageView.js
@@ -12,12 +12,28 @@ export class MessageView {
         <button id="endTurn">End turn</button>
         Match ID: <b><tt>${this.client.matchID}</tt></b>
         <br/><b>Click and drag</b> to pan, <b>mouse wheel</b> to zoom; <b>Q</b> and <b>E</e> to rotate card; <b>W</b> to move card to front/back.
-        <br>You have ${getClientCards(this.client).length} cards. &mdash; `;
+        <br>Players: `;
     
+    Object.entries(state.G.players).forEach(([playerId, playerData]) => {
+      console.log(playerId, playerData);
+      let str = `Player ${playerId}`;
+      if (playerId == this.client.playerID) {
+        str += ` (You)`
+      }
+      if (playerId == state.ctx.currentPlayer) {
+        str = `<b>${str}</b>`;
+      }
+      if (playerData) {
+        str += `: ${playerData.cards.length} cards; `;
+      } else {
+        str += `: not in room; `;
+      }
+      this.rootElement.innerHTML += str;
+    });
     if (state.ctx.currentPlayer == this.client.playerID) {
-      this.rootElement.innerHTML += "It's your turn!";
+      this.rootElement.innerHTML += "<br>It's your turn!";
     } else {
-      this.rootElement.innerHTML += `Wait, it's player ${state.ctx.currentPlayer}'s turn.`;
+      this.rootElement.innerHTML += `<br>Wait, it's player ${state.ctx.currentPlayer}'s turn.`;
     }
 
     document.getElementById('drawCard').onclick = (e) => {
