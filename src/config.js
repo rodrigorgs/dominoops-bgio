b4d75948951345ef2716af58a7c37fd643c85481
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-01-31 16:56:04 -0300

    Restores default cards per hand

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const GAME_NAME = "default";
 
 export const BOARD_WIDTH = 11;
 export const BOARD_HEIGHT = 11;
-export const CARDS_PER_HAND = 2;
+export const CARDS_PER_HAND = 4;
 
 export const NUM_CARDS = 30;
 export const CARD_WIDTH = 400;
