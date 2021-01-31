8dbe38fd3b0d027a4c6de15c49af992991e5cfa1
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-01-30 22:51:32 -0300

    Grows board

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const GAME_NAME = "default";
 
-export const BOARD_WIDTH = 8;
-export const BOARD_HEIGHT = 4;
-export const CARDS_PER_HAND = 10; //4;
+export const BOARD_WIDTH = 11;
+export const BOARD_HEIGHT = 11;
+export const CARDS_PER_HAND = 4;
 
 export const NUM_CARDS = 30;
 export const CARD_WIDTH = 400;
