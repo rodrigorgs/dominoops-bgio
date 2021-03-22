25603749a6bd58f951f51bf06974b6aaeb15fa47
Pedro Pontes <pontescpedro@gmail.com>
2021-03-22 12:31:05 -0300

    Implement postgres database connection

---
+++ database.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+
+const { DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME } = process.env;
+
+const databaseUrl = `postgres://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+
+const database = new Client({
+  connectionString: process.env.DATABASE_URL || databaseUrl,
+});
+
+database.connect(err => {
+  if (err) {
+    console.error(`Error connecting to database:\n  ${err}`);
+  } else {
+    console.log(`Successfully connected to database!`);
+  }
+});
+
+export { database };
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const { Server } = require('boardgame.io/server');
 import path from 'path';
 import serve from 'koa-static';
+
+require('dotenv').config();
+
+const { Server } = require('boardgame.io/server');
 const { Game } = require('./src/Game');
+
+const { database } = require('./database');
 
 const server = Server({ games: [Game] });
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, './dist');
-server.app.use(serve(frontEndAppBuildPath))
+server.app.use(serve(frontEndAppBuildPath));
 
 server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) => await serve(frontEndAppBuildPath)(
-      Object.assign(ctx, { path: 'index.html' }),
-      next
-    )
-  )
+  server.app.use(async (ctx, next) => await serve(frontEndAppBuildPath)(Object.assign(ctx, { path: 'index.html' }), next));
 });
