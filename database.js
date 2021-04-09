6b6dbf7be7296c4fe3b0f3f917c943d25c99b686
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-04-09 06:40:45 -0300

    Allow running Dominoops without a database

--- database.js
+++ database.js
@@ -1,17 +1,45 @@
 // const { Pool } = require('pg');
 import { PostgresStore } from 'bgio-postgres';
+import { Sequelize } from 'sequelize';
 
 const { DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME } = process.env;
 
-const databaseUrl = `postgres://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+let databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl && DATABASE_HOST) {
+  databaseUrl = `postgres://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+} else {
+  databaseUrl = `postgres://${process.env.USER}:@localhost/`
+}
 
-const database = new PostgresStore(process.env.DATABASE_URL || databaseUrl, {
-  ssl: true,
+const sequelizeOptions = {
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
     },
-  },
-});
+  }
+};
+const postgresStoreOptions = {
+  ssl: true,
+  ...sequelizeOptions
+};
 
-export { database };
+export async function connectToDatabase() {
+  const sequelize = new Sequelize(databaseUrl, sequelizeOptions);
+
+  console.log(`Will try to connect to database at ${databaseUrl}`);
+  return sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to database has been established successfully.');
+    return sequelize.close()
+      .then(() => {
+        console.log('will create PostgresStore');
+        return Promise.resolve(new PostgresStore(databaseUrl, postgresStoreOptions));
+      });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+    console.error('Will continue without using the database.');
+    return Promise.resolve(undefined);
+  });
+}
--- server.js
+++ server.js
@@ -5,16 +5,21 @@ require('dotenv').config();
 
 const { Server, FlatFile } = require('boardgame.io/server');
 const { Game } = require('./src/Game');
+const { connectToDatabase } = require('./database');
 
-const { database } = require('./database');
-
-const server = Server({ games: [Game], db: database });
 const PORT = process.env.PORT || 8000;
 
-// Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './dist');
-server.app.use(serve(frontEndAppBuildPath));
-
-server.run(PORT, () => {
-  server.app.use(async (ctx, next) => await serve(frontEndAppBuildPath)(Object.assign(ctx, { path: 'index.html' }), next));
+connectToDatabase().then((database) => {
+  if (database !== undefined) {
+    console.log('Connected to database');
+  }
+  const server = Server({ games: [Game], db: database });
+  
+  // Build path relative to the server.js file
+  const frontEndAppBuildPath = path.resolve(__dirname, './dist');
+  server.app.use(serve(frontEndAppBuildPath));
+  
+  server.run(PORT, () => {
+    server.app.use(async (ctx, next) => await serve(frontEndAppBuildPath)(Object.assign(ctx, { path: 'index.html' }), next));
+  });
 });
