b41f32b831e82e417c88022858233f060c00ea9a
Pedro Pontes <pontescpedro@gmail.com>
2022-06-09 18:33:42 -0300

    Make config based on cards file

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
+const { baseObjects } = require('./data/base-objects.js');
+
 export const GAME_NAME = 'dominoops';
 
 export const BOARD_WIDTH = 11;
 export const BOARD_HEIGHT = 11;
 export const CARDS_PER_HAND = 5;
 
-export const NUM_CARDS = 30;
+export const NUM_CARDS = baseObjects.length;
 export const CARD_WIDTH = 400;
 export const CARD_HEIGHT = 400;
 export const DECK_N_COLUMNS = 10;
